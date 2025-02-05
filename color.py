ca4d7a1734d1fee45f930bc50f7a9bb6823e4c63
Anthony <116965082+danthonyp@users.noreply.github.com>
2025-02-05 14:36:40 -0500

    Test2_colour.py

---
+++ color.py
@@ -0,0 +1,24 @@
+#Create a program that evalute a mactching colores of Candian flag.
+#feb/5/2025
+# -------------------------------------------------
+#Color variables.
+# -------------------------------------------------
+color1 = "red and black" 
+color2 = "red and white"
+color3 = "yellow and black"
+color4 = "blue and white"
+
+answer = input("What color is the Candian flag? :")
+
+if answer == color2:
+    print("Your are correct \N{grinning face}")
+elif answer == color2:
+    print("your incorrect \N{grinning face}")
+else:
+    print("invalid anser \N{angry face}")
+print("have a nice day")
+
+
+
+
+
