3846446a01acca2fed837f07368047cb8fd83e9c
Anthony <116965082+danthonyp@users.noreply.github.com>
2025-02-05 13:11:28 -0500

    remoteupdate_to_file_2025_2_5

--- remote_update.py
+++ remote_update.py
@@ -1,14 +1,10 @@
 import paramiko
 from getpass import getpass
-vm_list = [
-        {"hostname": "192.168.68.132",},
-        {"hostname": "192.168.1.126",},
-    ]
 vm_pass = getpass("Enterrootpassword: ")
 
 ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-ssh_client.connect(hostname= vm_list, username="root", password=vm_pass)
+ssh_client.connect(hostname="192.168.68.132", username='root', password=vm_pass)
 print("Connected successfully!")
 
 stdin, stdout, stderr = ssh_client.exec_command( 'ping -c 2 8.8.8.8')
